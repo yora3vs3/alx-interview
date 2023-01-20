fdd2828ecd73bc77c0f2bfeb8c8902d0da8a5e30
yora3vs3 <99631524+yora3vs3@users.noreply.github.com>
2023-01-20 12:59:59 +0300

    random

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -3,8 +3,8 @@ import random
 import sys
 from time import sleep
 import datetime
-random.seed(0)
-for i in range(40):
+
+for i in range(10000):
     sleep(random.random())
     sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
         random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
@@ -12,4 +12,3 @@ for i in range(40):
         random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
         random.randint(1, 1024)
     ))
-    sys.stdout.flush()
